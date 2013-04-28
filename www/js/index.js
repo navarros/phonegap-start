34f4856505cad045c4e98d3a198b48d027ec997e
navarros <navarros@gmail.com>
2013-04-28 03:06:00 +0300

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -46,4 +46,14 @@ var app = {
 
         console.log('Received Event: ' + id);
     }
+    
+    $('.link').live('tap', function() {
+    url = $(this).attr("rel");   
+    loadURL(url);
+     });
+
+    function loadURL(url){
+    navigator.app.loadUrl(url, { openExternal:true });
+    return false;
+} 
 };
